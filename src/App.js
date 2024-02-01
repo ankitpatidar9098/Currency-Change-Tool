7514f762049311b8a3444c9e74cc8f23275d52c5
Ankit Patidar <ankitpatidar01650@gmail.com>
2024-02-01 18:44:47 +0530

    change-background-image

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://media.istockphoto.com/id/1369016721/vector/finance-background-illustration-with-abstract-stock-market-information-and-charts-over-world.jpg?s=1024x1024&w=is&k=20&c=8X8wZMJ4lk32le36PBwHdOTs3iR5SHWD_mZX_0CaBJ0=')`,
+            backgroundImage: `url('https://images.unsplash.com/photo-1580519542036-c47de6196ba5?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
         }}
         
     >
